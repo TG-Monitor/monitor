3ed45eb57e9138569c6165db1386d7f2ee6ac39a
Daniel Weibel <danielmweibel@gmail.com>
2018-07-12 18:19:29 +0800

    Extend MonitorData interface with addX()/removeX() methods

--- src/main/java/ai/quantumsense/tgmonitor/monitor/data/MonitorData.java
+++ src/main/java/ai/quantumsense/tgmonitor/monitor/data/MonitorData.java
@@ -5,8 +5,22 @@ import java.util.Set;
 public interface MonitorData {
     Set<String> getPeers();
     void setPeers(Set<String> peers);
+    void addPeer(String peer);
+    void addPeers(Set<String> peers);
+    void removePeer(String peer);
+    void removePeers(Set<String> peers);
+
     Set<String> getPatterns();
     void setPatterns(Set<String> patterns);
+    void addPattern(String pattern);
+    void addPatterns(Set<String> patterns);
+    void removePattern(String pattern);
+    void removePatterns(Set<String> patterns);
+
     Set<String> getEmails();
     void setEmails(Set<String> emails);
+    void addEmail(String email);
+    void addEmails(Set<String> emails);
+    void removeEmail(String email);
+    void removeEmails(Set<String> emails);
 }
--- src/main/java/ai/quantumsense/tgmonitor/monitor/data/MonitorDataImpl.java
+++ src/main/java/ai/quantumsense/tgmonitor/monitor/data/MonitorDataImpl.java
@@ -30,6 +30,26 @@ public class MonitorDataImpl implements MonitorData {
     }
 
     @Override
+    public void addPeer(String peer) {
+        this.peers.add(peer);
+    }
+
+    @Override
+    public void addPeers(Set<String> peers) {
+        this.peers.addAll(peers);
+    }
+
+    @Override
+    public void removePeer(String peer) {
+        this.peers.remove(peers);
+    }
+
+    @Override
+    public void removePeers(Set<String> peers) {
+        this.peers.removeAll(peers);
+    }
+
+    @Override
     public Set<String> getPatterns() {
         return new LinkedHashSet<>(patterns);
     }
@@ -40,6 +60,26 @@ public class MonitorDataImpl implements MonitorData {
     }
 
     @Override
+    public void addPattern(String pattern) {
+        this.patterns.add(pattern);
+    }
+
+    @Override
+    public void addPatterns(Set<String> patterns) {
+        this.patterns.addAll(patterns);
+    }
+
+    @Override
+    public void removePattern(String pattern) {
+        this.patterns.remove(pattern);
+    }
+
+    @Override
+    public void removePatterns(Set<String> patterns) {
+        this.patterns.removeAll(patterns);
+    }
+
+    @Override
     public Set<String> getEmails() {
         return new LinkedHashSet<>(emails);
     }
@@ -47,5 +87,25 @@ public class MonitorDataImpl implements MonitorData {
     @Override
     public void setEmails(Set<String> emails) {
         this.emails = new LinkedHashSet<>(emails);
+    }
+
+    @Override
+    public void addEmail(String email) {
+        this.emails.add(email);
+    }
+
+    @Override
+    public void addEmails(Set<String> emails) {
+        this.emails.addAll(emails);
+    }
+
+    @Override
+    public void removeEmail(String email) {
+        this.emails.remove(email);
+    }
+
+    @Override
+    public void removeEmails(Set<String> emails) {
+        this.emails.removeAll(emails);
     }
 }
