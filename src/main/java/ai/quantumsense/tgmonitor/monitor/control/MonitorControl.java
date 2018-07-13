b3d22032e65016f00bae23f0854669d4990d02c4
Daniel Weibel <danielmweibel@gmail.com>
2018-07-13 13:31:47 +0800

    Remove start() and pause() methods from MonitorControl
    These methods are not really necessary, as new monitors are directly
    started when adding peers to MonitorData. Pausing the entire monitor is
    the same as stopping all the individual peer monitors, which is done by
    removing peers from MonitorData.

--- src/main/java/ai/quantumsense/tgmonitor/monitor/control/MonitorControl.java
+++ src/main/java/ai/quantumsense/tgmonitor/monitor/control/MonitorControl.java
@@ -3,10 +3,7 @@ package ai.quantumsense.tgmonitor.monitor.control;
 import ai.quantumsense.tgmonitor.monitor.logincode.LoginCodeReader;
 
 public interface MonitorControl {
-    MonitorState getState();
     void login(String phoneNumber);
     void logout();
-    void start();
-    void pause();
     String getPhoneNumber();
 }
--- src/main/java/ai/quantumsense/tgmonitor/monitor/control/MonitorControlImpl.java
+++ src/main/java/ai/quantumsense/tgmonitor/monitor/control/MonitorControlImpl.java
@@ -1,15 +1,11 @@
 package ai.quantumsense.tgmonitor.monitor.control;
 
-import static ai.quantumsense.tgmonitor.monitor.control.MonitorState.LOGGED_IN_PAUSED;
-import static ai.quantumsense.tgmonitor.monitor.control.MonitorState.LOGGED_IN_RUNNING;
-import static ai.quantumsense.tgmonitor.monitor.control.MonitorState.LOGGED_OUT;
-
 public class MonitorControlImpl implements MonitorControl {
 
-    private String phoneNumber = null;
-    private MonitorState state = LOGGED_OUT;
     private Authenticator authenticator;
     private Executor executor;
+    private String phoneNumber = null;
+    private boolean loggedIn = false;
 
     private static MonitorControl instance = null;
 
@@ -24,79 +20,25 @@ public class MonitorControlImpl implements MonitorControl {
     }
 
     @Override
-    public MonitorState getState() {
-        return state;
-    }
-
-    @Override
     public void login(String phoneNumber) {
-        switch (state) {
-            case LOGGED_IN_RUNNING:
-            case LOGGED_IN_PAUSED:
-                illegalState("Attempting to log in, but already logged in");
-                break;
-            case LOGGED_OUT:
-                authenticator.login(phoneNumber);
-                this.phoneNumber = phoneNumber;
-                state = LOGGED_IN_PAUSED;
-                break;
-        }
+        if (loggedIn)
+            throw new RuntimeException("Attempting to log in, but already logged in");
+        authenticator.login(phoneNumber);
+        this.phoneNumber = phoneNumber;
+        loggedIn = true;
     }
 
     @Override
     public void logout() {
-        switch (state) {
-            case LOGGED_IN_RUNNING:
-                pause();
-            case LOGGED_IN_PAUSED:
-                authenticator.logout();
-                phoneNumber = null;
-                state = LOGGED_OUT;
-                break;
-            case LOGGED_OUT:
-                illegalState("Attempting to log out, but already logged out");
-                break;
-        }
-    }
-
-    @Override
-    public void start() {
-        switch (state) {
-            case LOGGED_IN_RUNNING:
-                illegalState("Attempting to start monitor, but is already running");
-                break;
-            case LOGGED_IN_PAUSED:
-                executor.startAll();
-                state = LOGGED_IN_RUNNING;
-                break;
-            case LOGGED_OUT:
-                illegalState("Attempting to start monitor, but is not logged in");
-                break;
-        }
-    }
-
-    @Override
-    public void pause() {
-        switch (state) {
-            case LOGGED_IN_RUNNING:
-                executor.stopAll();
-                state = LOGGED_IN_PAUSED;
-                break;
-            case LOGGED_IN_PAUSED:
-                illegalState("Attempting to pause monitor, but is already paused");
-                break;
-            case LOGGED_OUT:
-                illegalState("Attempting to pause monitor, but is not logged in");
-                break;
-        }
+        if (!loggedIn)
+            throw new RuntimeException("Attempting to log out, but already logged out");
+        authenticator.logout();
+        phoneNumber = null;
+        loggedIn = false;
     }
 
     @Override
     public String getPhoneNumber() {
         return phoneNumber;
-    }
-
-    private void illegalState(String msg) {
-        throw new IllegalStateException(msg);
     }
 }
