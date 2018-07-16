e4d9d368ba6adf94d2216831f0904a6ab6435d70
Daniel Weibel <danielmweibel@gmail.com>
2018-07-16 17:53:00 +0800

    Adapt Telegram interface to new way of monitoring peers
    Namely, reading all the messages by a single Telethon process and then
    filtering out those messages of the peers that are currently set to be
    monitored by the TelegramImpl.

--- src/main/java/ai/quantumsense/tgmonitor/monitor/Telegram.java
+++ src/main/java/ai/quantumsense/tgmonitor/monitor/Telegram.java
@@ -1,73 +1,59 @@
 package ai.quantumsense.tgmonitor.monitor;
 
-import java.util.Set;
-
 public interface Telegram {
 
     /**
-     * Log the user in.
+     * Log system into Telegram with the specified user phone number.
      *
-     * If the user is already logged in, then a RuntimeException is thrown.
+     * If the system is already logged in, then a RuntimeException is thrown.
      *
      * @param phoneNumber Phone number associated with user's Telegram account.
      */
     void login(String phoneNumber);
 
     /**
-     * Log the user out.
+     * Log out system from Telegram.
      *
-     * If any monitors are still running, they are stopped by this method.
+     * If the system is currently reading messages, then reading is stopped
+     * before logging out.
      *
-     * If the user is already logged out, then this method does nothing.
+     * If the system is already logged out, then this method does nothing.
      */
     void logout();
 
     /**
-     * Test if the user is currently logged in.
-     *
-     * TODO: it might be possible that the user gets logged out by Telegram,
-     * that is, without an explicit call to logout(). In this case, isLoggedIn()
-     * would return "false", but the session file would still be there. A new
-     * call to login() would handle this correctly, but maybe this case still
-     * should be handled explicitly, if it occurs frequently in practice.
+     * Test if the system is currently logged in into Telegram.
      *
      * @return "true" if the user is logged in, and "false" otherwise.
      */
     boolean isLoggedIn();
 
     /**
-     * Start a monitor for the specified peer (group or channel).
+     * Start reading the incoming messages in the peers (groups and channels)
+     * that are currently set in the Peers entities.
      *
-     * If a monitor for this peer is already running, then a RuntimeException
-     * is thrown.
+     * If the system is already reading, a RuntimeException is thrown.
      *
-     * @param peer Username of a Telegram group or channel.
-     */
-    void start(String peer);
-
-    /**
-     * Stop the monitor for the specified peer (group or channel).
-     *
-     * If there is no monitor running for this peer, then a RuntimeException is
+     * If the system is not logged in into Telegram, a RuntimeException is
      * thrown.
-     *
-     * @param peer Username of a Telegram group or channel.
      */
-    void stop(String peer);
+    void startReading();
 
     /**
-     * Returns the number of monitors currently running.
+     * Stop reading the incoming messages in the peers (groups and channels)
+     * that are currently set in the Peers entities.
      *
-     * Note: there can be at most one monitor running for a given peer.
+     * If the system is currently not reading, a RuntimeException is thrown.
      *
-     * @return Number of currently running monitors.
+     * If the system is not logged in into Telegram, a RuntimeException is
+     * thrown.
      */
-    int getNumberOfMonitors();
+    void stopReading();
 
     /**
-     * Return usernames of peers for which currently a monitor is running.
+     * Test if the system is currently reading messages.
      *
-     * @return Usernames of monitored peers (groups or channels).
+     * @return "true" if currently reading, and "false" otherwise.
      */
-    Set<String> getMonitors();
+    boolean isReading();
 }
